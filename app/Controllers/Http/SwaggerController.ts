c63148e5a7602312aa3fc2483bebdc9be1b69769
diegodavilafreitas <diego.davilafreitas@gmail.com>
2025-02-07 16:10:15 +0100

    feat(swagger-controller): add insertResponses & insertHeaders

--- app/Controllers/Http/SwaggerController.ts
+++ app/Controllers/Http/SwaggerController.ts
@@ -7,16 +7,21 @@ import Project from 'App/Models/Project'
 import { swaggerMock } from 'App/SwaggerParser/mocks'
 import Route from 'App/Models/Route'
 import { ResponseInterface } from 'App/Interfaces/ResponseInterface'
+import Ws from 'App/Services/Ws'
+import Response from 'App/Models/Response'
+import { HeaderInterface } from 'App/Interfaces/HeaderInterface'
+import Header from 'App/Models/Header'
 
 export default class SwaggerController {
   public async parse({ auth, params, request, response }: HttpContextContract) {
-    console.log('PARAMS:', request)
+    await auth.authenticate()
     const data = await request.validate(ImportSwaggerValidator)
 
     const result = await parseSwagger(data.swagger)
     const resultMock = swaggerMock
-    //await this.insertRoutes(resultMock, params.id)
+    await this.insertRoutes(resultMock, params.id)
 
+    Ws.io.emit(`project:${params.id}`, `updated`)
     return response.created(result)
   }
 
@@ -31,21 +36,50 @@ export default class SwaggerController {
 
       for (const routeData of newRoutes) {
         const newRoute = await this.createNewRouteInRoot(project, false, routeData, trx)
-        await this.insertResponses(newRoute.responses, newRoute.id, trx)
+
+        if (routeData.responses && routeData.responses.length > 0) {
+          await this.insertResponses(newRoute.id, routeData.responses, false, trx)
+        }
       }
     })
   }
 
-  private async insertResponses(responses: ResponseInterface[], routeId: number, trx: any) {}
+  private async insertResponses(
+    routeId: number,
+    responses: ResponseInterface[],
+    isFile: boolean,
+    trx: any
+  ) {
+    for (const response of responses) {
+      const newResponse = await Response.create(
+        { ...response, isFile, routeId, body: response.body },
+        { client: trx }
+      )
 
-  // Helper functions
+      if (response.headers && response.headers.length > 0) {
+        await this.insertHeaders(response.headers, newResponse.id, trx)
+      }
+    }
+  }
+
+  private async insertHeaders(headers: HeaderInterface[], responseId: number, trx: any) {
+    for (const header of headers) {
+      await Header.create({ ...header, responseId }, { client: trx })
+    }
+  }
+
   private async createNewRouteInRoot(
     project: Project,
     isFolder: boolean,
     data: { [p: string]: any },
     trx: any
   ) {
-    const lastOrder = await project.related('routes').query().orderBy('order', 'desc').first()
+    const lastOrder = await project
+      .related('routes')
+      .query()
+      .useTransaction(trx)
+      .orderBy('order', 'desc')
+      .first()
     return project
       .related('routes')
       .create({ ...data, isFolder, order: (lastOrder?.order ?? 0) + 1 }, { client: trx })
