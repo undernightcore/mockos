1ed20cbae9f2316efaa3464f7800650151814425
diegodavilafreitas <diego.davilafreitas@gmail.com>
2025-03-03 21:18:35 +0100

    feat(responseController): add delete multiple responses

--- start/routes/responses.ts
+++ start/routes/responses.ts
@@ -1,6 +1,7 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
+  Route.delete('selected', 'ResponsesController.deleteMultiple')
   Route.get(':id', 'ResponsesController.get')
   Route.put(':id', 'ResponsesController.edit')
   Route.delete(':id', 'ResponsesController.delete')
