c0e890418d32db891a51437aa68ec0facbe0c656
Javier Martí Valcárcel <undernightcore@gmail.com>
2025-03-04 13:09:45 +0100

    feat: add validator and i18n error messages

--- start/routes/responses.ts
+++ start/routes/responses.ts
@@ -1,7 +1,6 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-  Route.delete('selected', 'ResponsesController.deleteMultiple')
   Route.get(':id', 'ResponsesController.get')
   Route.put(':id', 'ResponsesController.edit')
   Route.delete(':id', 'ResponsesController.delete')
